cbb0e53fc6ba65ba97968a0a3583fe12971bf8c9
yamagishi <yamagishi@gridsoalr.jp>
2017-10-04 10:42:40 +0900

    first conv vae with mnist

---
+++ src/mnist_vae.py
@@ -0,0 +1,78 @@
+# encoding:utf-8
+import renom as rm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from pdb import set_trace
+from numpy import random
+from time import time
+from skimage import io
+from glob import glob
+import gzip
+from vae_func import VGG_Enc, Dec, Vae2d
+from renom.cuda.cuda import set_cuda_active
+from numpy.random import seed, permutation
+
+
+def load(fname, shape, offset):
+    with gzip.open(fname, 'rb') as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=offset).reshape(shape)
+    return data
+
+if 0:
+    fls = glob('mnist/*.gz')
+    data = [[[],[]], [[],[]]]
+    for fname in fls:
+        shapes = [(-1, 1), (-1, 1, 28, 28)]
+        offsets = (8, 16)
+        type_idx = 0 if 'labels' in fname else 1
+        idx = 0 if 'train' in fname else 1
+        data[idx][type_idx] = load(fname, shapes[type_idx], offsets[type_idx])
+    data = np.array(data)
+    np.save('mnist/data.npy', data)
+else:
+    data = np.load('mnist/data.npy')
+
+y_train = data[0][0].astype('float32')/255.
+x_train = data[0][1].astype('float32')/255.
+y_test = data[1][0].astype('float32')/255.
+x_test = data[1][1].astype('float32')/255.
+
+
+set_cuda_active(True)
+seed(10)
+
+enc = VGG_Enc()
+dec = Dec()
+vae = Vae2d(enc, dec)
+loss = vae(x_train[:10])
+
+optimizer = rm.Adam()
+
+epoch = 10
+batch_size = 256
+N = len(x_train)
+curve = []
+for e in range(epoch):
+    perm = permutation(N)
+    batch_loss = []
+    for offset in range(0, N, batch_size):
+        idx = perm[offset: offset+batch_size]
+        s = time()
+        with vae.train():
+            grad = vae(x_train[idx]).grad()
+        s = time() - s
+        grad.update(optimizer)
+        batch_loss.append([
+            vae.kl_loss.as_ndarray()[0], 
+            vae.recon_loss.as_ndarray(), s])
+        loss_na = np.array(batch_loss)
+        kl_loss = loss_na[:,0].mean()
+        recon_loss = loss_na[:,1].mean()
+        s_mean = loss_na[:,2].mean()
+        print('{}/{} KL:{:.3f} ReconE:{:.3f} ETA:{:.1f}sec'.format(
+            offset, N, kl_loss, recon_loss, (N-offset)/batch_size*s_mean),
+            flush=True, end='\r')
+    curve.append([kl_loss, recon_loss])
+    print('#{} KL:{:.3f} ReconE:{:.3f} @ {:.1f}sec'.format(
+        e, kl_loss, recon_loss, loss_na[:,2].sum()))
--- src/vae_func.py
+++ src/vae_func.py
@@ -5,6 +5,158 @@ import matplotlib as mpl
 from pdb import set_trace
 from numpy import random
 from time import time
+
+class VGG_Enc(rm.Model):
+    def __init__(
+            self,
+            input_shape = (28, 28),
+            depth = 4,#28x28 -> 14x14 -> 7x7 -> 4x4 -> 2x2
+            batch_normal = False,
+            latent_dim = 10,
+            dropout = False
+        ):
+        self.depth = depth
+        self.batch_normal = batch_normal
+        self.input_shape = input_shape
+        self.latent_dim = latent_dim
+        self.dropout = dropout
+        parameters = []
+        channels = np.log(1024)/np.log(2)-self.depth + 1
+        boot_steps = 0
+        if channels <= 3:
+            boot_steps = 3 - channels
+            channels = 3
+        channels = int(2**channels)
+        for _ in range(boot_steps):
+            if self.batch_normal:
+                parameters.append(rm.Conv2d(channels, padding=(1,1)))
+                parameters.append(rm.BatchNormalize())
+                parameters.append(rm.Conv2d(channels, padding=(1,1)))
+                parameters.append(rm.BatchNormalize())
+            else:
+                parameters.append((rm.Conv2d(channels, padding=(1,1))))
+                parameters.append((rm.Conv2d(channels, padding=(1,1))))
+        for _ in range(self.depth - boot_steps):
+            if self.batch_normal:
+                parameters.append(rm.Conv2d(channels, padding=(1,1)))
+                parameters.append(rm.BatchNormalize())
+                parameters.append(rm.Conv2d(channels, padding=(1,1)))
+                parameters.append(rm.BatchNormalize())
+            else:
+                parameters.append((rm.Conv2d(channels, padding=(1,1))))
+                parameters.append((rm.Conv2d(channels, padding=(1,1))))
+            channels *= 2
+        self.hidden = rm.Sequential(parameters)
+        nb_parameters = int(
+                np.log(input_shape[0]*input_shape[1])
+                / np.log(self.depth)
+            )*1024
+        parameters = []
+        fcnn_depth = int((np.log(nb_parameters) - np.log(latent_dim))/np.log(4))
+        nb_parameters = nb_parameters // 4 
+        for _ in range(fcnn_depth):
+            parameters.append(rm.Dense(nb_parameters))
+            nb_parameters = nb_parameters // 4 
+        parameters.append(rm.Dense(latent_dim))
+        parameters.append(rm.Dense(latent_dim))
+        self.fcnn = rm.Sequential(parameters)
+
+    def forward(self, x):
+        layers = self.hidden._layers
+        for i in range(self.depth):
+            if self.batch_normal:
+                x = layers[i*4](x)
+                x = rm.relu(layers[i*4+1](x))
+                x = layers[i*4+2](x)
+                x = rm.relu(layers[i*4+3](x))
+            else:
+                x = rm.relu(layers[i*2](x))
+                #print(x.shape)
+                x = rm.relu(layers[i*2+1](x))
+                #print(x.shape)
+            x = rm.max_pool2d(x, stride=2, padding=(1,1))
+            #print(x.shape)
+        x = rm.flatten(x)
+        layers = self.fcnn._layers
+        for i in range(len(layers[:-2])):
+            x = rm.sigmoid(layers[i](x))
+            #print(x.shape)
+            if self.dropout:
+                x = rm.dropout(x, dropout_ratio=0.5)
+        z_mean = rm.tanh(layers[-2](x))
+        z_log_var = rm.tanh(layers[-1](x))
+        return z_mean, z_log_var
+
+class Dec(rm.Model):
+    def __init__(
+            self,
+            latent_dim = 10,
+            output_shape = (28, 28), # 正方行列を仮定
+            #28x28 <- 14x14 <- 7x7 <-* 1024x2x2
+            batch_normal = False,
+            dropout = False
+        ):
+        self.batch_normal = batch_normal
+        self.latent_dim = latent_dim
+        self.output_shape = output_shape
+        self.dropout = dropout
+        parameters = []
+        dim = output_shape[0]
+        channels = 1 # Gray
+        while dim%2 == 0 and dim > 2:
+            parameters.append(rm.Deconv2d(
+                channel=channels, stride=2, filter=2))
+            if batch_normal:
+                parameters.append(rm.BatchNormalize())
+            dim = dim // 2
+            channels *= 4
+        if dim%2 == 1:
+            parameters.append(rm.Deconv2d(
+                channel=channels, stride=2, filter=3))
+            dim = (dim - 1) // 2
+            channels *= 4
+        parameters.reverse()
+        self.hidden = rm.Sequential(parameters)
+        self.transform = rm.Dense(1024*1*dim*dim)
+        self.output = rm.Conv2d(channel=1,stride=1,filter=1)
+        self.dim = dim
+
+    def forward(self, x):
+        h = self.transform(x)
+        #print(h.shape)
+        h = rm.reshape(h, (len(x), 1024, self.dim, self.dim))
+        #print(h.shape)
+        layers = self.hidden._layers
+        for i in range(len(layers)):
+            if self.batch_normal:
+                h = layers[2*i](h)
+                h = rm.relu(layers[2*i+1](h))
+            else:
+                h = rm.relu(layers[i](h))
+            #print(h.shape)
+        h = rm.sigmoid(self.output(h))
+        return h
+
+class Vae2d(rm.Model):
+    def __init__(self, enc, dec):
+        self.enc = enc
+        self.dec = dec
+        self.latent_dim = enc.latent_dim
+
+    def forward(self, x):
+        self.z_mean, self.z_log_var = self.enc(x)
+        e = np.random.randn(len(x), self.latent_dim) * 1.
+        z_new = self.z_mean + rm.exp(self.z_log_var/2)*e
+        self.decoded = self.dec(z_new)
+        nb, zd = self.z_log_var.shape
+        self.kl_loss = - 0.5 * rm.sum(
+            1 + self.z_log_var - self.z_mean**2 - rm.exp(self.z_log_var)
+            )
+        self.recon_loss = rm.sigmoid_cross_entropy(self.decoded, x) 
+        vae_loss = self.kl_loss/nb + self.recon_loss
+        return vae_loss 
+
+    
 class DenseNet(rm.Model):
     def __init__(self,
         input_shape,

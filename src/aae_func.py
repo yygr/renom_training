9edbc41684c90d6cef2720c8bb10d9a2ba9ebc2e
Yamagishi Yohei <yamagishi@gridsolar.jp>
2017-11-02 19:06:16 +0900

    add aae_incorp

--- src/aae_func.py
+++ src/aae_func.py
@@ -8,39 +8,48 @@ from time import time
 from renom.utility.initializer import Gaussian, Uniform
 
 class AAE(rm.Model):
-    def __init__(self, latent_dim, prior='normal', prior_dist=None):
+    def __init__(
+            self, 
+            latent_dim, 
+            mode = 'normal',
+            label_dim = 0,
+            prior = 'normal', 
+            prior_dist = None,
+            hidden = 1000
+        ):
         self.latent_dim = latent_dim
+        self.mode = mode
+        self.label_dim = label_dim
         self.prior = prior
         self.prior_dist = prior_dist
         self.enc = rm.Sequential([
             rm.BatchNormalize(),
-            rm.Dense(200), rm.Relu(),
+            rm.Dense(hidden), rm.Relu(), #rm.Dropout(),
             rm.BatchNormalize(),
-            rm.Dense(100), rm.Relu(),
-            rm.Dense(latent_dim)
+            rm.Dense(hidden), rm.Relu(), #rm.Dropout(),
+            rm.Dense(latent_dim, initializer=Uniform())
         ])
         self.dec = rm.Sequential([
             rm.BatchNormalize(),
-            rm.Dense(10), rm.LeakyRelu(),
+            rm.Dense(hidden), rm.LeakyRelu(),
             rm.BatchNormalize(),
-            rm.Dense(100), rm.LeakyRelu(),
+            rm.Dense(hidden), rm.LeakyRelu(),
             rm.BatchNormalize(),
             rm.Dense(28*28), rm.Sigmoid()
         ])
         self.dis = rm.Sequential([
             rm.BatchNormalize(),
-            rm.Dense(100), rm.LeakyRelu(),
+            rm.Dense(hidden), rm.LeakyRelu(),
             rm.BatchNormalize(),
-            rm.Dense(100), rm.LeakyRelu(),
-            rm.BatchNormalize(),
-            rm.Dense(200), rm.LeakyRelu(),
+            rm.Dense(hidden), rm.LeakyRelu(),
+            #rm.BatchNormalize(),
+            #rm.Dense(hidden), rm.LeakyRelu(),
             rm.Dense(1), rm.Sigmoid()
         ])
-    def forward(self, x, eps=1e-3):
+    def forward(self, x, y=None, eps=1e-3):
         nb = len(x)
         self.z_mu = self.enc(x)
         self.recon = self.dec(self.z_mu)
-        self.reconE = rm.mean_squared_error(self.recon, x)
 
         if self.prior == 'normal':
             self.pz = np.random.randn(nb, self.latent_dim)
@@ -68,27 +77,86 @@ class AAE(rm.Model):
             types = np.random.randint(0, div, size=nb)
             for i in range(div):
                 idx = np.where(types==i)[0]
+                if len(idx) == 0:
+                    continue
                 rad = i * np.pi / (div/2)
                 mean = [np.cos(rad), np.sin(rad)]
-                start = [mean[0]*0.9, mean[1]*0.9+0.03]
-                end = [mean[0]*1.1+0.03, mean[1]*1.1]
+                start = [mean[0]*5, mean[1]*5]
+                end = [mean[1]*0.1,mean[0]*0.1]
+                mean = [mean[0]*5, mean[1]*5]
                 smplz[idx,0], smplz[idx,1] = np.random.multivariate_normal(
                     mean, [start,end], len(idx)).T
             self.pz = smplz
+            #plt.scatter(self.pz[:,0], self.pz[:,1], c=types.reshape(-1))
+            #plt.show()
+
         elif self.prior == 'predist':
             idx = np.random.permutation(len(self.prior_dist))
             self.pz = self.prior_dist[idx[:nb]]
-            
+
+        # label を埋め込んで使う場合
+        if self.mode == 'incorp_label':
+            pz_label = np.zeros((nb, self.label_dim+1))
+            types = np.random.randint(0, div+1, size=nb)
+            pz_label[np.arange(nb), types] = 1
+            perm = np.random.permutation(nb)
+            pz_full_mixture_idx = perm[:-nb//2]
+            pz_mixture_comp_idx = perm[-nb//2:]
+            unlabeled = np.zeros((1, self.label_dim+1))
+            unlabeled[0,-1] = 1
+            pz_label[pz_full_mixture_idx] = unlabeled
+            self.pz = np.c_[self.pz, pz_label]
+        if not y is None:
+            self.pzx = np.c_[self.z_mu, y]
+            pzx_full_mixture_idx = np.where(y[-1]==1)[0]
+            pzx_mixture_comp_idx = np.where(y[-1]==0)[0]
+
         self.Dispz = self.dis(self.pz)
-        self.Dispzx = self.dis(self.z_mu)
-        self.real = -rm.sum(rm.log(
-            self.Dispz + eps
-        ))/nb
-        self.fake = -rm.sum(rm.log(
-            1 - self.Dispzx + eps
-        ))/nb
-        self.enc_loss = -rm.sum(rm.log(
-            self.Dispzx + eps
-        ))/nb 
-        self.gan_loss = self.real + self.fake
+        self.Dispzx = self.dis(self.pzx)
+        if 0:#self.mode == 'incorp_label':
+            self.real_fm = -rm.sum(rm.log(
+                self.Dispz[pz_full_mixture_idx] + eps
+            ))/len(pz_full_mixture_idx)
+            self.real_mc = -rm.sum(rm.log(
+                self.Dispz[pz_mixture_comp_idx] + eps
+            ))/len(pz_mixture_comp_idx)
+            self.fake_fm = -rm.sum(rm.log(
+                1 - self.Dispzx[pzx_full_mixture_idx] + eps
+            ))/len(pzx_full_mixture_idx)
+            self.fake_mc = -rm.sum(rm.log(
+                1 - self.Dispzx[pzx_mixture_comp_idx] + eps
+            ))/len(pzx_mixture_comp_idx)
+            self.enc_loss_fm = -rm.sum(rm.log(
+                self.Dispzx[pzx_full_mixture_idx] + eps
+            ))/len(pzx_full_mixture_idx)
+            self.enc_loss_mc = -rm.sum(rm.log(
+                self.Dispzx[pzx_mixture_comp_idx] + eps
+            ))/len(pzx_mixture_comp_idx)
+            self.gan_loss_fm = self.real_fm + self.fake_fm
+            self.gan_loss_mc = self.real_mc + self.fake_mc
+            self.reconE_fm = rm.mean_squared_error(
+                self.recon[pzx_full_mixture_idx],
+                x[pzx_full_mixture_idx]
+            )
+            self.reconE_mc = rm.mean_squared_error(
+                self.recon[pzx_mixture_comp_idx],
+                x[pzx_mixture_comp_idx]
+            )
+            self.real = self.real_fm + self.real_mc
+            self.fake = self.fake_fm + self.fake_mc
+            self.gan_loss = self.gan_loss_fm + self.gan_loss_mc
+            self.enc_loss = self.enc_loss_fm + self.enc_loss_mc
+            self.reconE = self.reconE_fm + self.reconE_mc
+        else:
+            self.reconE = rm.mean_squared_error(self.recon, x)
+            self.real = -rm.sum(rm.log(
+                self.Dispz + eps
+            ))/nb
+            self.fake = -rm.sum(rm.log(
+                1 - self.Dispzx + eps
+            ))/nb
+            self.enc_loss = -rm.sum(rm.log(
+                self.Dispzx + eps
+            ))/nb 
+            self.gan_loss = self.real + self.fake
         return self.recon
---
+++ src/aae_incorplabel_mnist.py
@@ -0,0 +1,163 @@
+# encoding:utf-8
+import renom as rm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from pdb import set_trace
+from numpy import random
+from time import time
+from skimage import io
+from glob import glob
+import gzip
+from aae_func import AAE
+from renom.cuda.cuda import set_cuda_active
+from numpy.random import seed, permutation
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.manifold import TSNE
+from skimage import io, color
+
+seed(10)
+
+data = np.load('mnist/data.npy')
+
+y_train = data[0][0]
+x_train = data[0][1].astype('float32')/255.
+y_test = data[1][0]
+x_test = data[1][1].astype('float32')/255.
+x_train = x_train.reshape(-1, 28*28)
+x_test = x_test.reshape(-1, 28*28)
+
+def one_hot(data, size=11):
+    temp = np.zeros((len(data), size))
+    temp[np.arange(len(data)),data.reshape(-1)]=1
+    return temp
+
+y_train_1 = one_hot(y_train)
+idx = np.random.permutation(len(y_train))[:40000]
+y_train_1[idx] = np.r_[np.zeros(10),np.ones(1)].reshape(1,11)
+y_test_ = one_hot(y_test)
+
+latent_dim = 2
+epoch = 100
+batch_size = 256
+shot_freq = epoch//10
+
+train = True
+
+#ae = AAE(latent_dim, prior='uniform')
+if 1:
+    ae = AAE(latent_dim, hidden=1000, 
+        prior='10d-gaussians', mode='incorp_label', label_dim=10)
+    #ae = AAE(latent_dim, prior='gaussians')
+else:
+    im = io.imread('src/guri.png')
+    im = color.rgb2gray(im)
+    v, h = np.where(im==im.max())
+    v = -v/500 + 1.
+    h = h/500 - 1.
+    dist = np.c_[h, v]
+    ae = AAE(latent_dim, prior='predist', prior_dist=dist)
+
+"""
+dis_fm_opt = rm.Adam()
+dis_mc_opt = rm.Adam()
+enc_fm_opt = rm.Adam()
+enc_mc_opt = rm.Adam()
+"""
+dis_opt = rm.Adam()
+enc_opt = rm.Adam()
+
+N = len(x_train)
+curve = []
+for e in range(epoch):
+    if not train:
+        continue
+    perm = permutation(N)
+    batch_loss = []
+    for offset in range(0, N, batch_size):
+        idx = perm[offset: offset+batch_size]
+        s = time()
+        with ae.train():
+            ae(x_train[idx], y=y_train_1[idx])
+        with ae.enc.prevent_update():
+            l = ae.gan_loss
+            l.grad(detach_graph=False).update(dis_opt)
+            #l = ae.gan_loss_fm
+            #l.grad(detach_graph=False).update(dis_fm_opt)
+            #l = ae.gan_loss_mc
+            #l.grad(detach_graph=False).update(dis_mc_opt)
+        with ae.dis.prevent_update():
+            l = ae.enc_loss + ae.reconE
+            l.grad().update(enc_opt)
+            #l = ae.enc_loss_fm
+            #l.grad(detach_graph=False).update(enc_fm_opt)
+            #l = ae.enc_loss_mc
+            #l.grad(detach_graph=False).update(enc_mc_opt)
+        s = time() - s
+        batch_loss.append([
+            ae.gan_loss, ae.enc_loss, ae.reconE, 
+            ae.real, ae.fake, s])
+        loss_na = np.array(batch_loss)
+        gan_loss = loss_na[:,0].mean()
+        enc_loss = loss_na[:,1].mean()
+        recon_loss = loss_na[:,2].mean()
+        real = loss_na[:,3].mean()
+        fake = loss_na[:,4].mean()
+        s_mean = loss_na[:,-1].mean()
+        print_str = '>{:5d}/{:5d}'.format(offset, N)
+        print_str += ' Dis:{:.3f} Enc:{:.3f} ReconE:{:.3f}'.format(
+            gan_loss, enc_loss, recon_loss
+        )
+        print_str += ' {:.2f}/{:.2f}'.format(real, fake)
+        print_str += ' ETA:{:.1f}sec'.format((N-offset)/batch_size*s_mean)
+        print(print_str, flush=True, end='\r')
+    print_str = '#{:5d}/{:5d}'.format(e+1, epoch)
+    print_str += ' Dis:{:.3f} Enc:{:.3f} ReconE:{:.3f}'.format(
+        gan_loss, enc_loss, recon_loss
+    )
+    print_str += ' {:.2f}/{:.2f}'.format(real, fake)
+    print_str += ' @ {:.1f} sec {:>20}'.format(
+        loss_na[:,-1].sum(), ''
+    )
+    print(print_str)
+
+    if e % shot_freq != shot_freq - 1:
+        continue
+    res = ae.enc(x_test[:batch_size])
+    res = res
+    for i in range(batch_size, len(x_test), batch_size):
+        z_mean = ae.enc(x_test[i:i+batch_size])
+        res = np.r_[res, z_mean]
+    if latent_dim == 2 or e == epoch - 1:
+        if latent_dim != 2:
+            res = TSNE().fit_transform(res)
+        plt.clf()
+        plt.figure(figsize=(7,7))
+        plt.scatter(res[:,0], res[:,1], c=y_test.reshape(-1), alpha=0.5)
+        plt.axis('equal')
+        plt.savefig('result/AAEi_latent{}_{}.png'.format(latent_dim, e))
+        plt.savefig('result/AAEi_latent.png')
+
+    res_dim = 16
+    ims = ae(x_test[:batch_size], y_test_[:batch_size])
+    ims = ims.reshape(-1, 28, 28)
+    #og = x_test[:batch_size].reshape(-1, 28, 28)
+    cv = np.zeros((res_dim*28, res_dim*28))
+    for i in range(res_dim):
+        for j in range(res_dim):
+            idx = i*res_dim+j
+            cv[i*28:(i+1)*28, j*28:(j+1)*28] = ims[idx] #+ og[idx]
+    cv -= cv.min()
+    cv /= cv.max()
+    cv *= 255
+    cv = cv.astype('uint8')
+    io.imsave('result/AAEi{}_decode{}.png'.format(latent_dim, e), cv)
+    io.imsave('result/AAEi_decode.png', cv)
+"""
+if train:
+    enc.save('model/enc.h5')
+    dec.save('model/dec.h5')
+else:
+    enc.load('model/enc.h5')
+    dec.load('model/dec.h5')
+"""
